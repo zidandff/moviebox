4e64bde55ebe588d6924af232e717641cb725028
zidandff <zidandevs@gmail.com>
2023-01-11 22:32:43 +0700

    update navbar

--- src/script/components/navigation-bar.js
+++ src/script/components/navigation-bar.js
@@ -4,22 +4,38 @@ class NavigationBar extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <nav class="absolute w-full z-10 py-4">
-        <div class="container flex justify-between items-center">
-          <a class="navbar-brand" href="#">
-            <img src="${brandLogo}" alt="">
-          </a>
+        <div class="container">
+          <div class="nav-wrapper w-full flex justify-between items-center relative">
+            <div class="hamburger-menu">
+              <div class="hamburger-top"></div>
+              <div class="hamburger-middle"></div>
+              <div class="hamburger-bottom"></div>
+            </div>
+            
+            <form class="hidden rounded-lg border transition w-96 justify-between border-white px-4 py-2 lg:flex">
+              <input class="bg-transparent border-none p-0 pr-3 flex-1 text-gray-100 placeholder:text-gray-100 focus:ring-0" type="text" placeholder="What do you want to watch?">
+              <button class="ml-auto" type="submit">
+                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 14L10 10M11.3333 6.66667C11.3333 9.244 9.244 11.3333 6.66667 11.3333C4.08934 11.3333 2 9.244 2 6.66667C2 4.08934 4.08934 2 6.66667 2C9.244 2 11.3333 4.08934 11.3333 6.66667Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>          
+              </button>
+            </form>
 
-          <form class="rounded-lg border transition w-96 flex justify-between border-white px-4 py-2">
-            <input class="bg-transparent border-none p-0 pr-3 flex-1 text-gray-100 placeholder:text-gray-100 focus:ring-0" type="text" placeholder="What do you want to watch?">
-            <button class="ml-auto" type="submit">
-              <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 14L10 10M11.3333 6.66667C11.3333 9.244 9.244 11.3333 6.66667 11.3333C4.08934 11.3333 2 9.244 2 6.66667C2 4.08934 4.08934 2 6.66667 2C9.244 2 11.3333 4.08934 11.3333 6.66667Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>          
+            <a class="navbar-brand" href="#">
+              <img src="${brandLogo}" alt="">
+            </a>
+
+            <button class="toggle-search">
+              <svg width="24" height="24" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M14 14L10 10M11.3333 6.66667C11.3333 9.244 9.244 11.3333 6.66667 11.3333C4.08934 11.3333 2 9.244 2 6.66667C2 4.08934 4.08934 2 6.66667 2C9.244 2 11.3333 4.08934 11.3333 6.66667Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>          
             </button>
-          </form>
 
-          <button class="bg-primary rounded-full p-2">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" clip-rule="evenodd" d="M3.59998 8.40001C3.59998 7.73727 4.13723 7.20001 4.79998 7.20001H19.2C19.8627 7.20001 20.4 7.73727 20.4 8.40001C20.4 9.06275 19.8627 9.60001 19.2 9.60001H4.79998C4.13723 9.60001 3.59998 9.06275 3.59998 8.40001Z" fill="white"/><path fill-rule="evenodd" clip-rule="evenodd" d="M3.59998 15.6C3.59998 14.9373 4.13723 14.4 4.79998 14.4H19.2C19.8627 14.4 20.4 14.9373 20.4 15.6C20.4 16.2628 19.8627 16.8 19.2 16.8H4.79998C4.13723 16.8 3.59998 16.2628 3.59998 15.6Z" fill="white"/></svg>          
-          </button>
+            <form class="mobile-search absolute w-full top-full translate-y-4 rounded-lg border transition flex justify-between items-stretch border-white pl-4 pr-1 py-1 bg-[rgba(0,0,0,0.3)] backdrop-blur-md ">
+              <input class="bg-transparent border-none p-0 py-1 pr-3 flex-1 text-gray-100 placeholder:text-gray-100 focus:ring-0" type="text" placeholder="What do you want to watch?">
+              <button class="bg-primary rounded-md text-white text-sm px-3" type="submit">
+                Search
+              </button>
+            </form>
 
+          </div>
+          
         </div>
       </nav>
     `
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     container: {
       center: true,
       padding: {
-        lg: '60px'
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
       }
     },
     extend: {
